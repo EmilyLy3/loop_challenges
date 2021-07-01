3777e864c3d4ab7854832d34e95dafb24311534f
Emily Ly <em.a.ly63@gmail.com>
2021-07-01 18:13:47 -0400

    initial save

---
+++ loop_challenges.js
@@ -0,0 +1,48 @@
+// Print odds 1-20
+// Using a loop write code that will console.log all of the odd values from 1 up to 20.
+console.log('Challenge 1');
+
+for (i=1; i <=20; i+=2) {
+    console.log(i);
+}
+
+// Decreasing Multiples of 3
+// Using a loop write code that will console.log all of the values that are evenly divisible by 3 from 100 down to 0.
+console.log('Challenge 2');
+
+for (i=100; i>0; i--) {
+    if (i % 3 == 0) {
+        console.log(i);
+    }
+}
+
+// Print the sequence
+// Using a loop write code that will console.log the values in this sequence 4, 2.5, 1, -0.5, -2, -3.5.
+console.log('Challenge 3');
+
+for (i=4; i>= -3.5; i-=1.5) {
+    console.log(i);
+}
+
+// Sigma
+// Write code that will add all of the values from 1-100 onto some variable sum and at the end console.log the result 1 + 2 + 3 + ... + 98 + 99 + 100. We should get back 5050 at the end.
+console.log('Challenge 4');
+
+// var sum needs to be outside because if 'var sum = 0' was inside the loop, sum would be resetting to '0' at the beginning of each loop
+var sum = 0; 
+
+for (i=1; i<=100; i++) {
+    sum += i;
+}
+console.log(sum)
+
+// Factorial
+// Write code that will multiply all of the values from 1-12 onto some variable product and at the end console.log the result 1 * 2 * 3 * ... * 10 * 11 * 12. We should get back 479001600 at the end.
+console.log('Challenge 5');
+
+var product = 1;
+
+for (i=1; i<=12; i++) {
+    product = product*i;
+}
+console.log(product)
